3fb409b6fbd331038334d4b9fe6250f471cd82ef
kyang <kyang@stonedoorgroup.com>
2020-07-27 07:41:25 -0500

    update log

--- src/main/java/com/stonedoor/internal/DemoLoginApplication.java
+++ src/main/java/com/stonedoor/internal/DemoLoginApplication.java
@@ -8,6 +8,7 @@ public class DemoLoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoLoginApplication.class, args);
+		System.out.println("the endpoint is: /v1/api/demo/social/login");
 	}
 
 }
